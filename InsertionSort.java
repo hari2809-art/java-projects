675624deabe7c24d80ec0f90997f2f69cfb014c3
hari2809-art <gurramharinath28@gmail.com>
2025-06-16 09:51:33 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,24 @@
+package java_with_dsa;
+
+import java.util.*;
+
+public class InsertionSort {
+	public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        insertionSort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
